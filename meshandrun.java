977b671b9daa17b156f5732697d3950f5f587026
Marin Lauber <48692086+marinlauber@users.noreply.github.com>
2020-02-23 14:30:54 +0000

    Add files via upload

---
+++ meshandrun.java
@@ -0,0 +1,28 @@
+// STAR-CCM+ macro: meshandrun.java
+// Written by STAR-CCM+ 8.04.007
+package macro;
+
+import java.util.*;
+
+import star.common.*;
+import star.meshing.*;
+
+public class meshandrun extends StarMacro {
+
+  public void execute() {
+    execute0();
+  }
+
+  private void execute0() {
+
+    Simulation simulation_0 = 
+      getActiveSimulation();
+
+    MeshPipelineController meshPipelineController_0 = 
+      simulation_0.get(MeshPipelineController.class);
+
+    meshPipelineController_0.generateVolumeMesh();
+
+    simulation_0.getSimulationIterator().run();
+  }
+}
