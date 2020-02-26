7bc60b1fd3c92951d09d004a7f7850cecfa6a390
Marin Lauber <48692086+marinlauber@users.noreply.github.com>
2020-02-26 08:27:46 +0000

    Add files via upload

---
+++ stoppingcriterion.java
@@ -0,0 +1,28 @@
+// STAR-CCM+ macro: meshandrun.java
+// Written by STAR-CCM+ 12.04.01
+package macro;
+
+import java.util.*;
+
+import star.common.*;
+import star.base.neo.*;
+
+public class stoppingcriterion extends StarMacro {
+
+  public void execute() {
+    execute0();
+  }
+
+  private void execute0() {
+
+    Simulation simulation_0 = 
+      getActiveSimulation();
+
+    PhysicalTimeStoppingCriterion physicalTimeStoppingCriterion_0 = 
+      ((PhysicalTimeStoppingCriterion) simulation_0.getSolverStoppingCriterionManager().getSolverStoppingCriterion("Maximum Physical Time"));
+
+    physicalTimeStoppingCriterion_0.getMaximumTime().setValue(76.0);
+
+    simulation_0.getSimulationIterator().run();
+  }
+}
